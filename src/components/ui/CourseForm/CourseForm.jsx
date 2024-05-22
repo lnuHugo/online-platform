71484b02afd0e719c121fbd5916180ebb3ffac52
Hugo Barlind <hb223hu@student.lnu.se>
2024-05-22 09:45:36 +0200

    change initial value of youtubeLinks

--- src/components/ui/CourseForm/CourseForm.jsx
+++ src/components/ui/CourseForm/CourseForm.jsx
@@ -17,7 +17,7 @@ function CourseForm({
     price: data?.price ?? "",
     category: data?.category ?? "",
     image: data?.image ?? "",
-    youtubeLinks: data?.youtubeLinks ? data.youtubeLinks[0] : "",
+    youtubeLinks: Array.isArray(data?.youtubeLinks) ? data.youtubeLinks[0] : (data?.youtubeLinks ?? ""),
   });
 
   useEffect(() => {
