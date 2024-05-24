fb08bd44118745f7a0be96cac740e96b0545c979
Hugo Barlind <hb223hu@student.lnu.se>
2024-05-24 16:08:16 +0200

    Improve stating page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
-  const [testData, setTestData] = useState(null);
+  const [apiStatus, setApiStatus] = useState("disconnected");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await testApiResponse();
-        setTestData(data.message);
+        setApiStatus("connected");
       } catch (error) {
         console.error("There was an error fetching data:", error);
+        setApiStatus("disconnected");
       }
     };
 
@@ -21,13 +22,39 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <>
       <Header />
-      <div className="main-content">
-        <h1>{testData}</h1>
+      <div className="main-content start-main-div">
+        <div className={`status-box status-${apiStatus}`}>
+          <h1>
+            {apiStatus === "connected"
+              ? "Connected to backend server"
+              : "Disconnected from backend server"}
+          </h1>
+        </div>
+        <p>
+          Welcome to our application! This platform serves as a hub for various
+          courses and educational content. We are currently{" "}
+          {apiStatus === "connected" ? "connected to" : "disconnected from"} our
+          backend server, which may impact certain features. Please bear with us
+          as we work to establish a stable connection.
+        </p>
+        <p>
+          As you explore the site, feel free to browse our diverse range of
+          courses and resources. Whether you're a beginner or an expert, there's
+          something here for everyone. We're continuously updating and expanding
+          our offerings, so be sure to check back regularly for new content!
+        </p>
+        <p>
+          Please note that this application is still in its early stages and is
+          far from complete. Consider this version a starting point as we
+          continue to develop and improve the platform. Your feedback is
+          valuable to us as we strive to create the best possible experience for
+          our users.
+        </p>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
